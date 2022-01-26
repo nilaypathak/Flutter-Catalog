9b2d3fccd92b83a59698696fb907c1f3022d2767
Nilay <nilay.pathak.iitkgp@gmail.com>
2022-01-26 23:10:43 +0530

    Day 3 complete

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 // ignore_for_file: prefer_const_constructors
 
 import "package:flutter/material.dart";
-import 'package:flutter_catalog/home_page.dart';
+import 'package:flutter_catalog/pages/home_page.dart';
+import 'package:flutter_catalog/pages/login_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,11 +13,12 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    
-
-    // ignore: prefer_const_constructors
     return MaterialApp(
-      home: HomePage(),
-    );
+        theme: ThemeData(primarySwatch: Colors.deepPurple),
+        initialRoute: "/login",
+        routes: {
+          "/": (context) => HomePage(),
+          "/login": (context) => LoginPage(),
+        });
   }
 }
